13b9069db556432af316ba236d8eeec336e28bf8
“tlvu93” <101603072+tlvu93@users.noreply.github.com>
2022-05-31 13:37:36 +0200

    Add Route to set and get name

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import express from 'express';
+import session from 'express-session';
 
 // constants
 const app = express();
 const port = process.env.PORT || 3000;
 
+const sess = {
+  secret: '1O4HtWqqZ0',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {},
+};
+
+// Middlewares
+app.use(session(sess));
+
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-app.listen(3000);
+app.get('/setname', function (req, res) {
+  req.session.name = 'John Doe';
+  res.send(`Hello ${req.session.name}`);
+});
+
+app.get('/getname', function (req, res) {
+  res.send(req.session.name);
+});
+
+app.listen(port);
